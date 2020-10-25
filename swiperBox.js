000d89ffb3251292a0795b49aaeff98f2a173e7d
ManuXu <61727227+xuyihan-china@users.noreply.github.com>
2020-10-25 21:13:27 +0800

    00

---
+++ swiperBox.js
@@ -0,0 +1,32 @@
+<<<<<<< HEAD
+var swiper = new Swiper('.blog-slider', {
+    spaceBetween: 30,
+    effect: 'fade',
+    loop: true,
+    autoplay:true, 
+    delay:1000,
+    mousewheel: {
+      invert: true,
+    },
+    // autoHeight: true,
+    pagination: {
+      el: '.blog-slider__pagination',
+      clickable: true,
+    }
+  });
+=======
+var swiper = new Swiper('.blog-slider', {
+    spaceBetween: 30,
+    effect: 'fade',
+    loop: true,
+    mousewheel: {
+      invert: false,
+    },
+    // autoHeight: true,
+    pagination: {
+      el: '.blog-slider__pagination',
+      clickable: true,
+    }
+  });
+>>>>>>> 5917be00870a725271851accb7af541880cb0b6e
+  console.log("sucess")
